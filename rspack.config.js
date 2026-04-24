e49c8aa2d9969e64bc48863e12b457a6b4b1fee3
Rohit Srivastava <rohitcnfsecond@gmail.com>
2026-04-24 17:06:51 +0530

    testing

--- rspack.config.js
+++ rspack.config.js
@@ -1,11 +1,24 @@
 // @ts-check
 import { defineConfig } from '@rspack/cli';
 import { rspack } from '@rspack/core';
+import { ModuleFederationPlugin } from '@module-federation/enhanced/rspack';
 import { ReactRefreshRspackPlugin } from '@rspack/plugin-react-refresh';
+import { createRequire } from 'node:module';
 
 const isDev = process.env.NODE_ENV === 'development';
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
+  devServer: {
+    port: 3003,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  output: {
+    publicPath: 'auto',
+    uniqueName: 'mfe3',
+  },
   entry: {
     main: './src/main.jsx',
   },
@@ -47,6 +60,23 @@ export default defineConfig({
     ],
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'mfe3',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.jsx',
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: require('react/package.json').version,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: require('react-dom/package.json').version,
+        },
+      },
+    }),
     new rspack.HtmlRspackPlugin({
       template: './index.html',
     }),
